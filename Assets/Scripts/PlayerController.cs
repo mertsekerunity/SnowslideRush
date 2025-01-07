f1aaedff62bab30aeee6b5b2f6de89f137cf6558
mertsekerunity <mertseker9393@gmail.com>
2025-01-07 17:01:59 +0300

    Speed boost added.
    FindObjectOfType method added.
    Collision type changed to continuous in Skateboarder RB2D
    Speed boost added.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,46 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueValue = 1f;
+    [SerializeField] float normalSpeed = 20f;
+    [SerializeField] float boostSpeed = 30f;
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        RotatePlayer();
+        boostPlayer();
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             rb2d.AddTorque(torqueValue); //left direction
         }
         else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             rb2d.AddTorque(-torqueValue); //right direction
+        }
+    }
+
+    void boostPlayer()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = normalSpeed;
         }
     }
 }
