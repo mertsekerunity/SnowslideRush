db934a49f3fbab64aebe7a5b4b7c8b9496992136
mertsekerunity <mertseker9393@gmail.com>
2025-01-12 19:59:02 +0100

    jump method added.
    jump method added.
    jump particle system added.
    player controller script refactored.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float torqueValue = 1f;
     [SerializeField] float normalSpeed = 20f;
     [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float jumpSpeed = 10f;
 
+    [SerializeField] ParticleSystem jumpParticles;
+    bool isJumping;
+    bool isJumpKeyPressed;
+    bool isBoosting;
+    float rotationInput;
     bool canMove = true;
 
     Rigidbody2D rb2d;
@@ -25,13 +31,42 @@ public class PlayerController : MonoBehaviour
     {
         if (canMove)
         {
-            RotatePlayer();
-            boostPlayer();
+            //RotatePlayer();
+            //BoostPlayer();
+            //JumpPlayer();
+            (isJumping,isJumpKeyPressed) = CheckJumpInput();
+            isBoosting = CheckBoostInput();
+            rotationInput = CheckRotationInput();
+            if (isJumpKeyPressed && !jumpParticles.isPlaying) //doesnt work without jumpParticles.isPlaying, because of not proper init
+            {
+                jumpParticles.Play();
+            }
+            else if (!isJumpKeyPressed && jumpParticles.isPlaying) //doesnt work without jumpParticles.isPlaying, because of not proper init
+            {
+                jumpParticles.Stop();
+            }
         }
         
     }
 
-    void RotatePlayer()
+    private void FixedUpdate()
+    {
+        if (isJumping)
+        {
+            PerformJump();
+        }
+        if (isBoosting)
+        {
+            PerformBoostSpeed();
+        }
+        else
+        {
+            PerformNormalSpeed();
+        }
+        PerformRotation(rotationInput);
+    }
+
+    void RotatePlayer() //not advised approach
     {
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
@@ -43,7 +78,30 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void boostPlayer()
+    float CheckRotationInput()
+    {
+        // Return -1 for left, 1 for right, and 0 for no input
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            return -1f; // Left rotation
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            return 1f; // Right rotation
+        }
+
+        return 0f; // No rotation
+    }
+
+    void PerformRotation(float direction)
+    {
+        if (direction != 0)
+        {
+            rb2d.AddTorque(torqueValue * direction); // Rotate based on direction
+        }
+    }
+
+    void BoostPlayer() //not advised approach
     {
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
@@ -53,6 +111,32 @@ public class PlayerController : MonoBehaviour
         {
             surfaceEffector2D.speed = normalSpeed;
         }
+    }
+
+    bool CheckBoostInput()
+    {
+        return (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W));
+    }
+
+    void PerformBoostSpeed()
+    {
+        surfaceEffector2D.speed = boostSpeed;
+    }
+
+    void PerformNormalSpeed()
+    {
+        surfaceEffector2D.speed = normalSpeed;
+    }
+
+    (bool,bool) CheckJumpInput()
+    {
+        return ((Input.GetKey(KeyCode.Space) && !isJumping), (Input.GetKey(KeyCode.Space)));
+    }
+
+    void PerformJump()
+    {
+        rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed); // Set the vertical velocity 
+        isJumping = false; // Reset the flag after applying the jump
     }
 
     public void DisableControls()
